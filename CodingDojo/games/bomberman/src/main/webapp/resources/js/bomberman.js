e657c068d89a2221e54b75b71fc6c38ce7dbfff5
Denis Pankratov <bitbucket@dpankratov.ru>
2020-04-24 19:43:40 +0400

    Font and leaderlist are changed

--- CodingDojo/games/bomberman/src/main/webapp/resources/js/bomberman.js
+++ CodingDojo/games/bomberman/src/main/webapp/resources/js/bomberman.js
@@ -20,4 +20,132 @@
  * #L%
  */
 
-var doNothing = true;
+
+function initLeadersTable(contextPath, playerName, code, onDrawItem, onParseValue){
+    var leaderboard = $("#leaderboard");
+    leaderboard.css("width", "auto").css("font-size","inherit");
+    leaderboard.show();
+
+
+    function getAllValues(data) {
+        var result = {};
+        var players = Object.keys(data);
+        for (var index in players) {
+            var player = players[index];
+            var scores = data[player].scores;
+            var scoresPlayers = Object.keys(scores);
+            for (var index2 in scoresPlayers) {
+                var player2 = scoresPlayers[index2];
+                var score = scores[player2];
+                result[player2] = score;
+            }
+        }
+        return result;
+    }
+
+    function getReadableNames(data) {
+        var result = {};
+        var players = Object.keys(data);
+        for (var index in players) {
+            var player = players[index];
+            var names = data[player].heroesData.readableNames;
+            var namesPlayers = Object.keys(names);
+            for (var index2 in namesPlayers) {
+                var player2 = namesPlayers[index2];
+                var name = names[player2];
+                result[player2] = name;
+            }
+        }
+        return result;
+    }
+
+    function sortByScore(data) {
+        var vals = new Array();
+
+        for (i in data) {
+            var score = data[i];
+            if (!!onParseValue) {
+                score = onParseValue(score);
+            }
+            vals.push([i, score, data[i]])
+        }
+        vals = vals.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        var result = new Object();
+
+        for (i in vals) {
+            result[vals[i][0]] = vals[i][2];
+        }
+
+        return result;
+    }
+
+    function fromEmail(email) {
+        return email.split('@')[0];
+    }
+
+    function toName(email) {
+        return fromEmail(email.replace(' ', '&nbsp;'));
+    }
+
+    function drawLeaderTable(data) {
+        if (data == null) {
+            $("#table-logs-body").empty();
+            return;
+        }
+        var scores = getAllValues(data);
+        var readableNames = getReadableNames(data);
+        if (scores == null) {
+            $("#table-logs-body").empty();
+            return;
+        }
+
+        scores = sortByScore(scores);
+
+        if (!onDrawItem) {
+            onDrawItem = function(count, you, link, name, score) {
+                return '<tr>' +
+                        '<td>' + count + '</td>' +
+                        '<td>' + '<a href="' + link + '">' + name + '</a>' + you + '</td>' +
+                        '<td class="center">' + score + '</td>' +
+                    '</tr>';
+            }
+        }
+
+        var tbody = '';
+        var count = 0;
+        $.each(scores, function (email, score) {
+            var name = toName(readableNames[email]);
+
+            var you = '';
+            if (!!playerName) {
+                you = (name == toName(readableNames[playerName]))?"*":"";
+            }
+
+            count++;
+            var link = contextPath + '/board/player/' + email + ((!!code)?('?code=' + code):"");
+            tbody += onDrawItem(count, you, link, name, score);
+
+        });
+
+        $("#table-logs-body").empty().append(tbody);
+        leaderboard.trigger($.Event('resize'));
+    }
+
+    function isEmpty(map) {
+       for (var key in map) {
+          if (map.hasOwnProperty(key)) {
+             return false;
+          }
+       }
+       return true;
+    }
+
+    $('body').bind("board-updated", function(event, data) {
+        if (!isEmpty(data)) {
+            drawLeaderTable(data);
+        }
+    });
+};
